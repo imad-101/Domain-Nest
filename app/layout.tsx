266edc68cacec7ba9d2f1576016eed2d25afc956
imad-101 <imaduddin10.1973@gmail.com>
2025-08-26 16:47:10 +0500

    fixed plausible

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,6 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 {children}
 
                 </ModalProvider>
-              <Analytics />
               <Toaster richColors closeButton />
               <TailwindIndicator />
             </ThemeProvider>
